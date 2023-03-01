a9c968e0ed3a20c31b9f711844a6a02503008b20
Abhishek Das <abhidas0810@gmail.com>
2023-03-02 02:44:10 +0530

    login with jwt, powerUsage table and relationship with user

---
+++ powerUsageApp/databaseConfiguration/powerUsage.sql
@@ -0,0 +1,10 @@
+create TABLE powerUsage (
+    powerUsageId int primary key auto_increment,
+    fromTime datetime,
+    toTime datetime,
+    duration time,
+    unitConsumed int,
+    applianceType varchar(10),
+    userId int not null,
+    foreign key (userId) references users(userId)
+);
--- powerUsageApp/routes/users.routes.js
+++ powerUsageApp/routes/users.routes.js
@@ -1,29 +1,84 @@
+//requiring express
 const express = require("express");
+//requiring connection
 const connection = require("../serverAndConnection/connection");
+//requiring json web token
+const jwt = require("jsonwebtoken");
+//secretKey for json web token
+const secretKey = "powerUsageApp";
+//starting route
 const router = express.Router();
 
-router.post("/register", (req, res) => {
-  let user = req.body;
-  let query =
-    "insert into users (displayName,userName,emailId,mobileNumber,password) values (?,?,?,?,?)";
-  connection.query(
-    query,
-    [
-      user.displayName,
-      user.userName,
-      user.emailId,
-      user.mobileNumber,
-      user.password,
-    ],
-    (err, result) => {
-      if (!err) {
-        return res
-          .status(200)
-          .json({ message: "User registered successfully" });
+// REST API for user registration
+router.post("/register", async (req, res) => {
+  try {
+    // user data provided by user for registration
+    let user = req.body;
+    // query for adding new user record in database
+    let query =
+      "insert into users (displayName,userName,emailId,mobileNumber,password) values (?,?,?,?,?)";
+    // putting values in query
+    await connection.query(
+      query,
+      [
+        user.displayName,
+        user.userName,
+        user.emailId,
+        user.mobileNumber,
+        user.password,
+      ],
+      (err, result) => {
+        if (!err) {
+          // returning successful response
+          return res
+            .status(200)
+            .json({ message: "User registered successfully" });
+        }
+        // returning error response if occured while posting data in database
+        return res.status(400).json(err.message);
       }
-      return res.status(400).json(err.message);
-    }
-  );
+    );
+  } catch (error) {
+    // returning error response if occured inside try block
+    return res.status(400).json({ message: "something went wrong" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    let user = req.body;
+    // query for finding user in database
+    let query =
+      "select * from users where userName = ? or emailId = ? or mobileNumber = ?";
+    await connection.query(
+      query,
+      [user.userName, user.emailId, user.mobileNumber],
+      (err, result) => {
+        if (
+          !err &&
+          result.length != 0 &&
+          user.password === result[0].password
+        ) {
+          // UserID as payload
+          let userId = user.userId;
+          // genrating token with 5 minutes validity
+          const jwtToken = jwt.sign({ userId }, secretKey, {
+            expiresIn: "300s",
+          });
+          // returning successful response with token
+          return res.status(200).json({ token: jwtToken });
+        } else if (!err) {
+          // returning error response if password is wrong
+          return res.status(403).json({ message: "invalid password" });
+        }
+        // returning error response if error found while checking identity
+        return res.status(403).json({ message: "invalid credentials" });
+      }
+    );
+  } catch (error) {
+    // returning error response if occured inside try block
+    return res.status(400).json({ message: "something went wrong" });
+  }
 });
 
 module.exports = router;
